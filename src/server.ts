2c2ffbe8d8e59f10e54a23754ea4b2fa946700ed
Marcio Carolino <marciocarolino@gmail.com>
2023-05-18 17:29:17 -0300

    testando rotas basicas

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/test", (request, response) => {
+  return response.send("Olá Mundo!");
+});
+
+app.post("/test-post", (request, response) => {
+  return response.send("Ola Metodo POST");
+});
+
 app.listen(3000, () => console.log(`Server is running!`));
